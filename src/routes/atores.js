843beef69b82c09862fad7f6ea69d306a10cc86f
GabrielRodriguesScarpinella <gabriel.scarpinella@aluno.ifsp.edu.br>
2026-09-03 19:46:40 -0300

    Finalização de usuarios.js ecomeço de /atores/participa

--- src/routes/atores.js
+++ src/routes/atores.js
@@ -47,4 +47,57 @@ router.post("/", async (req, res) => {
     }
 });
 
+router.get("/participa", async (req, res)=>{
+    try{
+        //const r=await db.query("SELECT filmes.titulo, filmes.sinopse, atores.nome, atores.nacionalidade FROM filmes_atores JOIN atores ON atores.id = filmes_atores.idAtor JOIN filmes ON filmes.id = filmes_atores.idFilme")
+        const r=await db.query("SELECT atores.id idator, atores.nome ator, atores.nacionalidade, filmes.id idfilme, filmes.titulo FROM filmes_atores JOIN atores ON atores.id=filmes_atores.idAtor JOIN filmes ON filmes.id=filmes_atores.idFilme");
+        let lapf=[];
+        /*
+        lapf=[
+            {
+            idator:
+            ator:
+            nacionalidade:
+            filmes:[
+                {
+                idfilme:
+                titulo:
+                }
+            ]
+            },
+            {...}
+        ]
+        */
+        for(let i=0; i<r.rows.length; i++){//f-a
+            let atorConsta=false;
+            for(let j=0; j<lapf.length; j++){//a
+                if(r.rows[i].ator==lapf[j].ator){
+                    atorConsta=true;
+                    lapf[j].filmes.push({
+                        idFilme:idFilme,
+                        titulo:titulo
+                    })
+                    break;
+                }
+            }
+            if(!atorConsta){
+                lapf.push(
+                {
+                    idator:idator,
+                    ator:ator,
+                    filmes:[
+                        {
+                            idfilme:idfilme,
+                            titulo:titulo
+                        }
+                    ]
+                });
+            }
+        }
+        res.status(200).json({c:lapf});
+    }catch(erro){
+        res.status(400).json({msg:"Bixou"+erro});
+    }
+});
+//falta GET /participa/:idator
 module.exports = router;
--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -4,7 +4,7 @@ const db = require("../db");
 
 router.get("/", async (req, res) => {
 	try{
-		const r=await db.query("SELECT * FROM usuarios");
+		const r=await db.query("SELECT id, nome, email, critico, administrador, img FROM usuarios");
 		if(r.rowCount==0){
 			res.status(400).json({msg:"Não há usuários."});
 		}
@@ -15,82 +15,121 @@ router.get("/", async (req, res) => {
 });
 router.get("/:id", async (req, res) => {
 	try{
-		const {id}=req.params.id||{};
+		const id=req.params.id||{};
 		if(!id){throw new Error("Id não identificado!");}
-		const r=await db.query("SELECT * FROM usuarios WHERE id=$1", [id]);
+		const r=await db.query("SELECT id, nome, email, critico, administrador, img FROM usuarios WHERE id=$1", [id]);
 		if(r.rowCount==0){
 			res.status(400).json({msg:"Não há usuários."});
 		}
-		res.status(200).json({usuarios:r.rows});
+		res.status(200).json({usuario:r.rows[0]});
 	}catch(erro){
 		res.status(400).json({msg:erro});
 	}
 });
 router.post("/", async (req, res) => {
-	/*
-    nome VARCHAR(85) NOT NULL,
-    login VARCHAR(85) NOT NULL UNIQUE,
-    critico BOOLEAN DEFAULT FALSE,
-    administrador BOOLEAN DEFAULT FALSE,
-    img TEXT DEFAULT NULL,
-    senha VARCHAR(60) NOT NULL,
-    email VARCHAR(85) NOT NULL UNIQUE
-	*/
 	try{
-		const {nome, login, critico, administrador, img, senha, email}=req.body||{};
+		const {nome, critico, administrador, img, senha, email}=req.body||{};
 		if(!nome){throw new Error("Nome não identificado!");}
-		if(!login){throw new Error("Login não identificado!");}
 		if(!senha){throw new Error("Senha não identificada!");}
 		if(!email){throw new Error("E-mail não identificado!");}
-		const r=await db.query("INSERT INTO usuarios (nome, login, critico, administrador, img, senha, email) VALUES ($1, $2, $3, $4, $5, $6, $7)", [nome, login, critico, administrador, img, senha, email]);
+		const criticoBool=(critico==1234)?true:false;//código de crítico:1234
+		const administradorBool=(administrador==5678)?true:false;//código de administrador:5678
+		const r=await db.query("INSERT INTO usuarios (nome, critico, administrador, img, senha, email) VALUES ($1, $2, $3, $4, $5, $6)", [nome, criticoBool, administradorBool, img, senha, email]);
 		if(r.rowCount==0){
 			res.status(400).json({msg:"Não foi adicionado usuário."});
 		}
-		res.status(200).json({usuarios:r.rows});
+		res.status(200).json({msg:"Usuário adicionado com sucesso!"});
 	}catch(erro){
 		res.status(400).json({msg:erro});
 	}
 });
-//put falta
-
-/*
 router.put("/:id", async (req, res) => {
 	try{
-		const {id}=req.params.id||{};
-		const r=await db.query("SELECT * FROM usuario WHERE id=$1", [id]);
-		if(r.rowCount==0){
+		const id=req.params.id||{};
+		const rp=await db.query("SELECT * FROM usuarios WHERE id=$1", [id]);
+		if(rp.rowCount==0){
 			res.status(400).json({msg:"Usuário não encontrado."});
 		}
 		if(!id){throw new Error("Id não especificado!");}
-		const {nome}=req.body.nome||{r.usuarios[0].nome}
-		if(!r.usuarios.nome){throw new Error("Nome não identificado!");}
-		if(!login){throw new Error("Login não identificado!");}
-		if(!critico){throw new Error("Login não identificado!");}
-		if(!administrador){throw new Error("Login não identificado!");}
-		if(!img){throw new Error("Login não identificado!");}
-		if(!senha){throw new Error("Senha não identificada!");}
-		if(!email){throw new Error("E-mail não identificado!");}
-		const r2=await db.query("INSERT INTO usuario (nome, login, critico, administrador, img, senha, email) VALUES ($1, $2, $3, $4, $5, $6, $7)", [nome, login, critico, administrador, img, senha, email]);
-		if(r2.rowCount==0){
-			res.status(400).json({msg:"Não foi adicionado usuário."});
+		
+		if(req.body.nome){console.log("Nfdabusduid")}
+
+		let nome;
+		if(req.body.nome){
+			nome=req.body.nome;
+			let r=await db.query("UPDATE usuarios SET nome=$1 WHERE id=$2", [nome, id]);
+			if(r.rowCount==0){
+				res.status(400).json({msg:"Não foi alterado o nome do usuário."});
+			}
 		}
-		res.status(200).json({usuarios:r.rows});
+		let critico;
+		if(req.body.critico){
+			critico=(req.body.critico==1234)?true:false;
+			let r=await db.query("UPDATE usuarios SET critico=$1 WHERE id=$2", [critico, id]);
+			if(r.rowCount==0){
+				res.status(400).json({msg:"Não foi alterado o estado de crítico do usuário."});
+			}
+		}
+		let administrador;
+		if(req.body.administrador){
+			administrador=(req.body.administrador==5678)?true:false;
+			let r=await db.query("UPDATE usuarios SET administrador=$1 WHERE id=$2", [administrador, id]);
+			if(r.rowCount==0){
+				res.status(400).json({msg:"Não foi alterado o estado de administrador do usuário."});
+			}
+		}
+		let img;
+		if(req.body.img){
+			img=req.body.img;
+			let r=await db.query("UPDATE usuarios SET img=$1 WHERE id=$2", [img, id]);
+			if(r.rowCount==0){
+				res.status(400).json({msg:"Não foi alterado a imagem do usuário."});
+			}
+		}
+		let senha;
+		if(req.body.senha){
+			senha=req.body.senha;
+			let r=await db.query("UPDATE usuarios SET senha=$1 WHERE id=$2", [senha, id]);
+			if(r.rowCount==0){
+				res.status(400).json({msg:"Não foi alterado a senha do usuário."});
+			}
+		}
+		let email;
+		if(req.body.email){
+			email=req.body.email;
+			let r=await db.query("UPDATE usuarios SET email=$1 WHERE id=$2", [email, id]);
+			if(r.rowCount==0){
+				res.status(400).json({msg:"Não foi alterado o e-mail do usuário."});
+			}
+		}
+		res.status(200).json({msg:"Atualização realizada com sucesso."});
 	}catch(erro){
 		res.status(400).json({msg:erro});
 	}
 });
-*/
-
 router.delete("/:id", async (req, res) => {
 	try{
-		const {id}=req.params.id||{};
+		const id=req.params.id||{};
 		if(!id){throw new Error("Id não identificado!");}
 		const r=await db.query("DELETE * FROM usuarios WHERE id=$1", [id]);
 		if(r.rowCount==0){
 			res.status(400).json({msg:"Não foram apagados usuários."});
 		}
-		res.status(200).json({usuarios:r.rows});
+		res.status(200).json({msg:"Usuário removido com sucesso!"});
 	}catch(erro){
+		res.status(400).json({msg:erro});
+	}
+});
+
+router.post("/login", async (req, res)=>{
+	try{
+		const {email, senha}=req.body||{};
+		if(!email){throw new Error("E-mail não infromado.");}
+		if(!senha){throw new Error("Senha não informada.");}
+		const r=await db.query("SELECT id, nome, email, critico, administrador, img FROM usuarios WHERE email=$1 AND senha=$2", [email, senha]);
+		res.status(200).json({usuario:r.rows[0]});
+	}
+	catch(erro){
 		res.status(400).json({msg:erro});
 	}
 });
