a7bab2c437eb057d50ae6c34372b4ff720e4b303
JoaoSuriano17 <joao.suriano@aluno.ifsp.edu.br>
2026-09-03 13:55:02 -0300

    Mudanças no init.sql

--- init.sql
+++ init.sql
@@ -9,6 +9,8 @@ CREATE TABLE IF NOT EXISTS atores(
     qtde_premios INT DEFAULT 0,
 
     CONSTRAINT nascimento_CK CHECK (nascimento BETWEEN '1895-12-28T00:00:00Z' AND '2026-09-09T00:00:00Z')/*Primeira exposição pelos irmãos Lumière*/
+    CONSTRAINT qtde_premios_CK CHECK (qtde_premios >= 0 AND qtde_premio < 1000),
+
 );
 
 CREATE TABLE IF NOT EXISTS diretores(
@@ -31,30 +33,32 @@ CREATE TABLE IF NOT EXISTS filmes(
     orcamento INT NOT NULL,
     duracao INT NOT NULL,
 
-    CONSTRAINT diretor_FK FOREIGN KEY (diretor) REFERENCES diretores(id)
+    CONSTRAINT diretor_FK FOREIGN KEY (diretor) REFERENCES diretores(id),
+    CONSTRAINT orcamento_CK CHECK (orcamento > 0),
+    CONSTRAINT duracao_CK CHECK (duracao > 0),
+    CONSTRAINT faixa_etaria_CK CHECK (faixa_etaria > 0 AND <= 18)
 );
 
 CREATE TABLE IF NOT EXISTS filmes_atores(
-    id SERIAL PRIMARY KEY,
     idFilme INT,
     idAtor INT,
 
     CONSTRAINT ator_FK FOREIGN KEY (idAtor) REFERENCES atores(id),
-    CONSTRAINT filme_FK FOREIGN KEY (idFilme) REFERENCES filmes(id)
+    CONSTRAINT filme_FK FOREIGN KEY (idFilme) REFERENCES filmes(id),
+
+    CONSTRAINT filmes_atores_PK PRIMARY KEY (idFilme, idAtor)
 );
 
 
 CREATE TABLE IF NOT EXISTS usuarios(
     id SERIAL PRIMARY KEY,
     nome VARCHAR(85) NOT NULL,
-    login VARCHAR(85) NOT NULL UNIQUE,
+    email VARCHAR(85) NOT NULL UNIQUE,
     critico BOOLEAN DEFAULT FALSE,
     administrador BOOLEAN DEFAULT FALSE,
-    img TEXT,
+    img TEXT DEFAULT NULL,
     senha VARCHAR(60) NOT NULL,
-    email VARCHAR(85) NOT NULL UNIQUE,
 
-    CONSTRAINT login_CK CHECK (login NOT LIKE '% %' AND login NOT LIKE '%,%' AND login NOT LIKE '%;%'),/*Ou seja, sem espaços, vírgulas, ou ponto e vírgula;*/
     CONSTRAINT senha_CK CHECK (senha NOT LIKE '% %'),/*Ou seja, sem espaços*/
     CONSTRAINT email_CK CHECK (email LIKE '%@%.%' AND email NOT LIKE '% %' AND email NOT LIKE '%,%' AND email NOT LIKE '%;%')/*algo@algo.algo*/ /*Ou seja, sem espaços, vírgulas, ou ponto e vírgula;*/
 );
@@ -63,11 +67,13 @@ CREATE TABLE IF NOT EXISTS resenhas(
     id SERIAL PRIMARY KEY,
     idUsuario INT,
     idFilme INT,
-    resenha TEXT,
+    resenha TEXT NOT NULL,
     avaliacao DECIMAL(5,2),
 
     CONSTRAINT usuario_FK FOREIGN KEY (idUsuario) REFERENCES usuarios(id),
-    CONSTRAINT filme_FK FOREIGN KEY (idFilme) REFERENCES filmes(id)
+    CONSTRAINT filme_FK FOREIGN KEY (idFilme) REFERENCES filmes(id),
+    CONSTRAINT avaliacao_CK CHECK (avaliacao > 0 AND avaliacao <= 100),
+
 
 );
 
