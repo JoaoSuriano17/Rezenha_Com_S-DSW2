170020fb6c38c3070a275867d074c9456d4efab0
JoaoSuriano17 <joao.suriano@aluno.ifsp.edu.br>
2026-09-03 16:51:51 -0300

    Começo do desenvolvimento da rota /filmes/participa

--- src/routes/filmes.js
+++ src/routes/filmes.js
@@ -2,9 +2,51 @@ const express=require("express")
 const db=require("../db")
 const router=express()
 
+//Get - geral para filmes
 router.get("/", async (req, res)=>{
     try{
         const r=await db.query("SELECT * FROM filmes")
+        if (r.rowCount==0){
+            return res.json("Bixou, nenhum filme cadastrado")
+        }
+        res.json(r.rows)
+    }catch(erro){
+        res.json(erro)
+    }
+})
+
+//GET que mostra todos os filmes e seus atores
+router.get("/participa", async (req, res)=>{
+    try{
+        const r=await db.query("SELECT f.id idFilme, f.titulo, a.id idAtor, a.nome ator, a.nacionalidade FROM filmes_atores fa JOIN atores a ON a.id = fa.idAtor JOIN filmes f ON f.id = fa.idFilme")
+        //res.send(r.rows)
+
+        let filmes = []
+        let qia=0;
+        for (let contFilme = 0; contFilme<r.rows.length; contFilme++){
+            if (contFilme > 0){
+                if (filmes[qia-1].id == r.rows[contFilme].id){
+                    filmes[qia-1].atores.push({id: r.rows[contFilme].idator, ator: r.rows[contFilme].ator, nacionalidade: r.rows[contFilme].nacionalidade})
+                }else{
+                    let filme = {id: r.rows[contFilme].idFilme, titulo: r.rows[contFilme].titulo, atores: []}
+                    filmes.push(filme)
+                    qia++
+                }
+            }else{
+                let filme = {id: r.rows[contFilme].idfilme, titulo: r.rows[contFilme].titulo, atores: []}
+                filmes.push(filme)
+                qia++
+            }
+        }
+
+
+
+        //Adicionar o primeira autor -> 
+
+        
+            
+        res.json(filmes)
+
         if (r.rowCount==0){
             return res.json("Bixou, nenhum filme cadastrado")
         }
